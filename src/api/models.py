214f597fde6e65c21b5ce63920d229816c43d966
Даниил Козлов <zanzydnd@gmail.com>
2022-04-16 20:29:48 +0300

    docker-base

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=200)
+
+
+class Comment(BaseModel):
+    username = models.CharField(max_length=200)
+    text = models.TextField()
+    answer_to = models.ForeignKey("self", on_delete=models.CASCADE, related_name='answers')

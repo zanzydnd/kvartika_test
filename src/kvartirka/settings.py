0d1043a74a41a806290b88f8084bc647e54be87c
Даниил Козлов <zanzydnd@gmail.com>
2022-04-29 12:57:58 +0300

    didnt delete silk

--- src/kvartirka/settings.py
+++ src/kvartirka/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'kvartirka.urls'
